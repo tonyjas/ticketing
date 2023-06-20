b7e77378b4f77bff4457e25be722fc368b06f9d0
Tony Jasynewycz <tony_jas@hotmail.com>
2023-06-20 21:33:18 +0100

    Updates

---
+++ common/src/middlewares/current-user.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+interface UserPayload {
+  id: string;
+  email: string;
+}
+
+// Modify the existing Request interface adding the currentUser (UserPayload) interface optionally
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: UserPayload;
+    }
+  }
+}
+
+export const currentUser = (req: Request, res: Response , next: NextFunction) => {
+
+  if(!req.session?.jwt) {
+    return next();
+  }
+
+  try {
+    const payload = jwt.verify(
+      req.session.jwt,
+      process.env.JWT_KEY!
+    ) as UserPayload;
+
+    req.currentUser = payload;
+  
+  } catch (err) {}
+
+  next();
+
+}
